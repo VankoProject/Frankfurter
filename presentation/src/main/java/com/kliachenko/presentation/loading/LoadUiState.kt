a3a253016b8ad3b45c0390d11c34e63c02d2a3a6
Ivan Kliachenko <ivan.green69@gmail.com>
2024-02-25 18:26:39 +0200

    FF-03 loadUiState

--- presentation/src/main/java/com/kliachenko/presentation/loading/LoadUiState.kt
+++ presentation/src/main/java/com/kliachenko/presentation/loading/LoadUiState.kt
@@ -2,23 +2,32 @@ package com.kliachenko.presentation.loading
 
 interface LoadUiState {
 
-    fun update()
+    fun map(mapper: Mapper)
+
+    interface Mapper {
+
+        fun mapSuccess(data: List<String>)
+
+        fun mapError(message: String)
+    }
 
     object Progress: LoadUiState {
-        override fun update() {
-            TODO("Not yet implemented")
-        }
+        override fun map(mapper: Mapper) = Unit
     }
 
     data class Error(private val message: String): LoadUiState {
-        override fun update() {
-            TODO("Not yet implemented")
+        override fun map(mapper: Mapper) {
+            mapper.mapError(message)
         }
+    }
 
+    data class Success (private val data: List<String>): LoadUiState {
+        override fun map(mapper: Mapper) {
+            mapper.mapSuccess(data)
+        }
     }
 
     object Empty: LoadUiState {
-        override fun update() = Unit
-
+        override fun map(mapper: Mapper) = Unit
     }
 }
