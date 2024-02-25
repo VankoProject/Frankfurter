719258587e17b20bc8500e706d3adadec2fce82e
Ivan Kliachenko <ivan.green69@gmail.com>
2024-02-25 12:07:32 +0200

    FF-02 loadViewModel

--- presentation/src/main/java/com/kliachenko/presentation/loading/LoadViewModel.kt
+++ presentation/src/main/java/com/kliachenko/presentation/loading/LoadViewModel.kt
@@ -2,8 +2,10 @@ package com.kliachenko.presentation.loading
 
 import com.kliachenko.presentation.core.BaseViewModel
 import com.kliachenko.presentation.core.Clear
+import com.kliachenko.presentation.core.Navigation
 import com.kliachenko.presentation.core.RunAsync
 import com.kliachenko.presentation.core.UpdateUi
+import com.kliachenko.presentation.dashboard.DashBoardScreen
 
 class LoadViewModel(
     private val observable: LoadUiObservable,
@@ -14,11 +16,26 @@ class LoadViewModel(
 ) : BaseViewModel(runAsync) {
 
     fun init() {
-
+        observable.updateUi(LoadUiState.Progress)
+        if(repository.categories().isEmpty) {
+            load()
+            navigation.updateUi(DashBoardScreen)
+        } else {
+            navigation.updateUi(DashBoardScreen)
+        }
     }
 
     fun retry() {
+        load()
+    }
 
+    private fun load() {
+        observable.updateUi(LoadUiState.Progress)
+        runAsync({
+            repository.loadData()
+        }) { loadResult ->
+            loadResult.handle(observable)
+        }
     }
 
     fun startGettingUpdates(observer: UpdateUi<LoadUiState>) {
